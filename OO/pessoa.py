02a045e991b1f12f9abf1a17953e686b72ffb10e
peustratt <peustratt@gmail.com>
2020-12-05 19:12:01 -0300

    Criado método comprimentar

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def comprimentar(self):
+        return 'Hello'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    Pessoa.comprimentar(p)
+    print(p.comprimentar())
